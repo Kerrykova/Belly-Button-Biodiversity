f9174cb40e91ef7d334b1a3187d7f0fbe3c639a4
KerryKova <kerrykovacik@gmail.com>
2019-03-01 21:23:21 -0600

    adding project data

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,7 @@ function buildMetadata(sample) {
   var panel;
 
   d3.json(`/metadata/${sample}`).then(function(response){
-    console.log(response);
+  
     metasample = response;
     // Use d3 to select the panel with id of `#sample-metadata`
     panel = d3.select("#sample-metadata");
@@ -27,6 +27,7 @@ function buildMetadata(sample) {
 
 function buildCharts(sample) {
   var sampleData;
+  var sample = 940
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   d3.json(`/samples/${sample}`).then(function(response){
     // @TODO: Build a Bubble Chart using the sample data
@@ -34,8 +35,56 @@ function buildCharts(sample) {
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-    const top = sampleData.sample_values.slice(0, 11);
-    console.log(top);
+    var sample = sampleData.sample_values;
+    var otu = sampleData.otu_ids;
+    var labels = sampleData.otu_labels;
+
+    // PIE CHART
+
+    var topSamples = sample.slice(0, 10);
+    var topOtu = otu.slice(0,10);
+    var topLabels = labels.slice(0,10);
+
+    var pieData = [{
+      values: topSamples,
+      labels: topOtu,
+      type: 'pie',
+      hovertext: topLabels,
+      hoverinfo: 'label+text+value+percent',
+    }];
+    
+    var pieLayout = {
+      height: 500,
+      width: 500
+    };
+    
+    Plotly.newPlot('pie', pieData, pieLayout);
+
+    // BUBBLE CHART
+    var trace1 = {
+      x: otu,
+      y: sample,
+      text: labels,
+      mode: 'markers',
+      marker: {
+        color: otu,
+        opacity: [1, 0.8, 0.6, 0.4],
+        size: sample
+      }
+    };
+    
+    var data = [trace1];
+    
+    var layout = {
+      title: 'Belly Button Biodiversity',
+      showlegend: false,
+      height: 500,
+      width: 1200
+    };
+    
+    Plotly.newPlot('bubble', data, layout);
+    
+
   });
 }
 
